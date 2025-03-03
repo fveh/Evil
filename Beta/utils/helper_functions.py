069fe1a7804ffebd36bcac90ca2c3692ada6c3a3
veh <fabianveh@gmx.de>
2025-03-03 02:29:47 +0100

    Update helper_functions.py

--- Beta/utils/helper_functions.py
+++ Beta/utils/helper_functions.py
@@ -1,1 +1,10 @@
+import logging
 
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        filename='evil_beta.log',
+        filemode='a'
+    )
+    logging.info("Logging initialisiert.")
